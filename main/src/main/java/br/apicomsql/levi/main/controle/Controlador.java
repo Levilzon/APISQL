e48a4a777877c05a0d151382c527f21511f81842
Levilzon <levilima2002@gmail.com>
2023-09-07 15:43:55 -0300

    Adicionados as funções de deletar e contador de entidades

--- main/src/main/java/br/apicomsql/levi/main/controle/Controlador.java
+++ main/src/main/java/br/apicomsql/levi/main/controle/Controlador.java
@@ -3,6 +3,7 @@ package br.apicomsql.levi.main.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,18 @@ public class Controlador {
         return acao.save(obj);
     }
 
+    @DeleteMapping("/lista/delete/{id}")
+    public void remover(@PathVariable int id){
+        Perfil obj = selecionarId(id);
+        acao.delete(obj);
+
+    }
+
+    @GetMapping("/lista/contador")
+    public long contador(){
+        return acao.count();
+
+    }
     
     
 }
