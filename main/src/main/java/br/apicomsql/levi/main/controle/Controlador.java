e99372e09f51f54a795bd9c99d83d6cfcadb2949
Jose Levi Gonçalves de Lima Menezes <levilima2002@gmail.com>
2023-10-09 11:32:17 -0300

    Adicionado ferramenta de pesquisa de iniciais e de final de letra do final do nome, mais comentários sobre as funções dos controladores e das funções do repositório

--- main/src/main/java/br/apicomsql/levi/main/controle/Controlador.java
+++ main/src/main/java/br/apicomsql/levi/main/controle/Controlador.java
@@ -18,48 +18,62 @@ import br.apicomsql.levi.main.repositorio.Repositorio;
 @RestController
 @RequestMapping(value = "/pcaminho")
 public class Controlador {
-    
+    //conexão das ações do repositório com o controlador
     @Autowired
     private Repositorio acao;
-    
+    //Função para saber se a API está funcionando
     @GetMapping("/teste")
     public String olaMundo(){
         return "ola mundo";
     }
-
+    //vai mostrar as informações add no banco 
     @GetMapping("/lista")
     public List<Perfil> selecionar(){
         return acao.findAll();
     }
-
+    //vai listar as informações do banco por ordem alfabética
+    @GetMapping("/lista/ordenarnomes")
+    public List<Perfil> ordenarPorNome(){
+        return acao.findByOrderByNome();
+    }
+    //usado para pesquisar a entidade por meio do ID
     @GetMapping("/lista/{id}")
     public Perfil selecionarId(@PathVariable int id){
         return acao.findById(id);
     }
-
+    //vai editar a informação de alguma entidade
     @PutMapping("/salvar")
     public Perfil editar(@RequestBody Perfil obj){
         return acao.save(obj);
 
     }
-
+    //vai salvar uma nova entidade
     @PostMapping("/salvar")
     public Perfil cadastrar(@RequestBody Perfil obj){
         return acao.save(obj);
     }
-
+    //vai deletar uma entidade
     @DeleteMapping("/lista/delete/{id}")
     public void remover(@PathVariable int id){
         Perfil obj = selecionarId(id);
         acao.delete(obj);
 
     }
-
+    //vai contar a quantidade de entidades que tem presente no banco
     @GetMapping("/lista/contador")
     public long contador(){
         return acao.count();
 
     }
-    
+    //vai mostrar a entidades que possuem tal inicial
+    @GetMapping("/lista/pesquisarinicial/{termo}")
+    public List<Perfil> pesquisarInicialDoNome(@PathVariable String termo){
+        return acao.findByNomeStartsWith(termo);
+    }
+    //vai mostrar as entidades que possuem a letra final que for utilizada
+    @GetMapping("/lista/pesquisarfinal/{termo}")
+    public List<Perfil> pesquisarComLetraFinalDoNome(@PathVariable String termo){
+        return acao.findByNomeEndsWith(termo);
+    }
     
 }
--- main/src/main/java/br/apicomsql/levi/main/repositorio/Repositorio.java
+++ main/src/main/java/br/apicomsql/levi/main/repositorio/Repositorio.java
@@ -9,9 +9,15 @@ import br.apicomsql.levi.main.perfil.Perfil;
 
 @Repository
 public interface Repositorio extends CrudRepository<Perfil, Integer>{
-
+    //vai listar as informações
     List<Perfil> findAll();
+    //está sendo usado para pesquisar uma pessoa por meio do id
     Perfil findById(int id);
-
+    //usado para organizar os nomes em ordem alfabética 
+    List<Perfil> findByOrderByNome();
+    //vai listar apenas pessoas que possuem a inicial escolhida
+    List<Perfil> findByNomeStartsWith(String termo);
+    //usado para listar apenas pessoas com a letra final do nome(variavel nome)
+    List<Perfil> findByNomeEndsWith(String termo);
     
 }
